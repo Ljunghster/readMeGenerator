e86a44bcc7ae980096ee7a6b59b3dd144dfe0f5c
Ljunghster <mpljungh@gmail.com>
2020-05-15 00:02:27 -0700

    a little progress

--- Develop/index.js
+++ Develop/index.js
@@ -1,25 +1,52 @@
-axios({
-    method: "get",
-url: `https://api.github.com/users/${username}/repos`,
-headers: {
-    Authorization: 'Bearer ${githubToken}',
-    "Content-Type": "application/json"
-},
-auth: {
-    username: 'user',
-    password: 'password'
-}
-})
-.then()
-const questions = [
+const axios = require('axios')
+const inquirer = require('inquirer')
+const fs = require('fs');
 
+
+const questions = [
+    {
+        type: 'input',
+        name: 'comments',
+        message: 'Any comments on your purchase experience?',
+        default: 'Nope, all good!'
+      }
 ];
 
 function writeToFile(fileName, data) {
+    fs.writeFile(filename, data)
 }
 
-function init() {
 
+function init() {
+    inquirer.prompt(questions).then(answers => {
+
+        console.log(JSON.stringify(answers, null, '  '));
+      });
 }
 
 init();
+
+
+
+
+
+
+
+
+
+
+
+
+// axios({
+    //     method: "get",
+    // url: `https://api.github.com/users/${username}/repos`,
+    // headers: {
+    //     Authorization: 'Bearer ${githubToken}',
+    //     "Content-Type": "application/json"
+    // },
+    // auth: {
+    //     username: 'user',
+    //     password: 'password'
+    // }
+    // })
+    // .then()
